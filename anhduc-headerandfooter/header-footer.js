a7c1c8559437a804b7621462234c6237fb09d1d0
justnothin <vuleanhduc1604@gmail.com>
2023-04-04 23:18:55 +0700

    responsive update

--- anhduc-headerandfooter/header-footer.js
+++ anhduc-headerandfooter/header-footer.js
@@ -1,7 +1,15 @@
 var searchIcon = document.querySelector('.searchTab');
 var searchBox = document.querySelector('.search');
+var homeWrapper = document.querySelector('.home-wrapper');
+var serviceWrapper = document.querySelector('.service-wrapper');
+var teamWrapper = document.querySelector('.team-wrapper');
+var aboutWrapper = document.querySelector('.about-wrapper');
 searchIcon.addEventListener("click", activateSearch);
 function activateSearch() {  
   searchBox.classList.toggle("active-search");
   searchIcon.classList.toggle('active-searchTab');
+  homeWrapper.classList.toggle('active-search-wrapper');
+  serviceWrapper.classList.toggle('active-search-wrapper');
+  teamWrapper.classList.toggle('active-search-wrapper');
+  aboutWrapper.classList.toggle('active-search-wrapper');
 }
